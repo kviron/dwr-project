2d999a1f690f6da6a42a4069d9f9089da72de65f
Kviron <kviron.web.dev@gmail.com>
2020-12-29 19:46:27 +0300

    Удаление излишних файлов

--- web/app/themes/miu/components/city.php
+++
@@ -1,3 +0,0 @@
-<a href="" class="header__city-link link link_icon-map">
-    <?php the_field('city', 'contacts') ?>
-</a>
--- web/app/themes/miu/components/logotype.php
+++
@@ -1,7 +0,0 @@
-<a href="<?= home_url() ?>" class="icon-svg icon-svg_logotype">
-    <?php if (get_field('logo', 'options')) : ?>
-        <?= file_get_contents(get_field('logo', 'options')) ?>
-
-    <?php endif; ?>
-
-</a>
