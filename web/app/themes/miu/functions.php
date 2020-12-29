d7fd5f4d32524adfb662e6c0165c844b7d9c2098
Kviron <kviron.web.dev@gmail.com>
2020-12-29 18:44:46 +0300

    Правки шаблона сайта

--- web/app/themes/miu/functions.php
+++ web/app/themes/miu/functions.php
@@ -5,7 +5,7 @@ require_once get_template_directory()."/settings/_index.php";
 
 require_once get_template_directory()."/post-types/_index.php";
 
-require_once get_template_directory()."/taxonomies/_index.php";
+require_once get_template_directory()."/post-taxonomies/_index.php";
 
 require_once get_template_directory()."/includes/_index.php";
 
