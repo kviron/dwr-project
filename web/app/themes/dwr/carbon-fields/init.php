e4f5922a3453896c17b9a3b0445657b41c2e1e0b
kviron <kviron.web.dev@gmail.com>
2021-02-22 18:42:00 +0300

    последние правки

---
+++ web/app/themes/dwr/carbon-fields/init.php
@@ -0,0 +1,4 @@
+<?php
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
--- web/app/themes/dwr/functions.php
+++ web/app/themes/dwr/functions.php
@@ -1,4 +1,6 @@
 <?php
+require_once get_template_directory()."/carbon-fields/init.php";
+
 require_once get_template_directory()."/settings/_index.php";
 
 require_once get_template_directory()."/post-types/_index.php";
