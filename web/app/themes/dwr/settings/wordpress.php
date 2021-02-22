fd4e9d9de297c2264107d77cc7938bc4c4f7ca32
kviron <kviron.web.dev@gmail.com>
2021-02-22 18:04:58 +0300

    удаление лишних настроек

--- web/app/themes/dwr/settings/wordpress.php
+++ web/app/themes/dwr/settings/wordpress.php
@@ -57,15 +57,7 @@ function show_svg_in_media_library( $response ) {
 add_filter('transient_update_plugins', 'update_active_plugins');    // Hook for 2.8.+
 //add_filter('option_update_plugins', 'update_active_plugins');    // Hook for 2.7.x
 function update_active_plugins( $value = '' ){
-    /*
-    The $value array passed in contains the list of plugins with time
-    marks when the last time the groups was checked for version match
-    The $value->reponse node contains an array of the items that are
-    out of date. This response node is use by the 'Plugins' menu
-    for example to indicate there are updates. Also on the actual
-    plugins listing to provide the yellow box below a given plugin
-    to indicate action is needed by the user.
-    */
+
     if( (isset($value->response)) && (count($value->response)) ){
 
         // Get the list cut current active plugins
