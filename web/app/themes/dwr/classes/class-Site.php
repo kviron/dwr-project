cc866aa8931ac9edaa44582c066a77d4ff94a1db
kviron <kviron.web.dev@gmail.com>
2021-03-26 09:50:04 +0300

    small editing

--- web/app/themes/dwr/classes/class-Site.php
+++ web/app/themes/dwr/classes/class-Site.php
@@ -1,5 +1,5 @@
 <?php
-
+use function Env\env;
 
 class Site {
     /**
@@ -8,26 +8,40 @@ class Site {
      */
     public static $theme = [];
 
+    /**
+     * @var array
+     * Params teheme
+     */
+    public static $vars = [];
+
 
     /**
      * Set init params
      */
     public static function init(){
-        self::setUrlTheme(get_template_directory());
-        self::setPathTheme(get_template_directory_uri());
+        self::setThemeUrl(get_template_directory());
+        self::setThemePath(get_template_directory_uri());
+        self::setThemeName(get_template_directory_uri());
     }
 
     /**
      * Get url site
      */
-    public static function setUrlTheme($url_site){
+    public static function setThemeUrl($url_site){
         self::$theme['url'] = $url_site;
+    }
+
+    /**
+     * Get url site
+     */
+    public static function setThemeName($url_site){
+        self::$theme['name'] = env('WP_THEME_NAME');
     }
 
     /**
      * Get path theme
      */
-    public static function setPathTheme($path_theme){
+    public static function setThemePath($path_theme){
         self::$theme['path'] = $path_theme;
     }
 
--- web/app/themes/dwr/custom-fields/init.php
+++
@@ -1,4 +0,0 @@
-<?php
-use Carbon_Fields\Container;
-use Carbon_Fields\Field;
-
